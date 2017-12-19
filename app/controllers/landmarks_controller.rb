4cbe5a1ddb67ae6b7844aa31b4094789dcffae15
Kristen Peck <kpeck71@gmail.com>
2017-12-19 22:24:53 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,55 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Figure.all
+
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @figure = Figure.find(params[:id])
+
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  post '/landmarks' do
+    @figure = Figure.create(params["figure"])
+     if !params[:landmark][:name].empty?
+       @figure.landmarks << Landmark.create(params[:landmark])
+     end
+
+     if !params[:title][:name].empty?
+       @figure.titles << Title.create(params[:title])
+     end
+
+     @figure.save
+
+    redirect("/landmarks/#{@figure.id}")
+  end
+
+  post '/landmarks/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+
+   redirect("/landmarks/#{@figure.id}")
+  end
 end
