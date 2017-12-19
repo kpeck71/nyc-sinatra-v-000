e9cbf1497f50992112534e6f56b1f2a2c2749665
Kristen Peck <kpeck71@gmail.com>
2017-12-19 22:33:55 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -24,7 +24,7 @@ class LandmarksController < ApplicationController
   post '/landmarks' do
     @landmark = Landmark.create(params["landmark"])
      if !params[:landmark][:name].empty?
-       @landmark.name = Landmark.create(name: params[:landmark[:name])
+       @landmark.name = Landmark.create(name: params[:name])
      end
 
      if !params[:year_completed].empty?
