8caae8ab9674503d8c127ddbec6953e44ba4b1fd
Kristen Peck <kpeck71@gmail.com>
2017-12-19 22:53:30 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -22,7 +22,7 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks' do
-    @landmark = Landmark.create(params["landmark"]
+    @landmark = Landmark.create(params["landmark"])
 
     @landmark.save
 
