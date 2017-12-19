c39cbeaaa75c6566dff12862e411eee9eb350013
Kristen Peck <kpeck71@gmail.com>
2017-12-19 22:11:55 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -36,7 +36,7 @@ class FiguresController < ApplicationController
     erb :'/figures/edit'
   end
 
-  post '/figures/:id' do
+  patch '/figures/:id' do
     @figure = Figure.find(params[:id])
     @figure.update(params[:figure])
 
