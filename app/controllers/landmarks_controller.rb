785336d3ec5058602c1e1c6a317d6198fcf2225b
Kristen Peck <kpeck71@gmail.com>
2017-12-19 22:55:52 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,48 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params["landmark"])
+
+    @landmark.save
+
+    redirect("/landmarks/#{@landmark.id}")
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+
+    if !params[:name].nil?
+      @landmark.name = params[:name]
+    end
+
+    if !params[:year_completed].nil?
+      @landmark.year_completed = params[:year_completed]
+    end
+
+    @landmark.save
+
+   redirect("/landmarks/#{@landmark.id}")
+  end
 end
