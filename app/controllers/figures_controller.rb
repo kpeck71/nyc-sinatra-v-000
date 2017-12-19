0e283d385161730a588b9a0560204f348dcded9a
Kristen Peck <kpeck71@gmail.com>
2017-12-19 22:22:38 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -41,11 +41,11 @@ class FiguresController < ApplicationController
     @figure.update(params[:figure])
 
     if !params[:landmark][:name].empty?
-      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+      @figure.landmarks << Landmark.create( params[:landmark])
     end
 
     if !params[:title][:name].empty?
-      @figure.titles << Title.create(name: params[:title][:name])
+      @figure.titles << Title.create(params[:title])
     end
 
     @figure.save
